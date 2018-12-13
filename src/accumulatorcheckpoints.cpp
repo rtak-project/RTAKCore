5fff89bd49bee13cfad8fc6d9ec9eb0217e08343
RtakDev <45839765+rtak-dev@users.noreply.github.com>
2018-12-14 01:42:06 +0300

    Update accumulatorcheckpoints.cpp

--- src/accumulatorcheckpoints.cpp
+++ src/accumulatorcheckpoints.cpp
@@ -4,7 +4,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "accumulatorcheckpoints.h"
-#include "accumulatorcheckpoints.json.h"
+#include "accumulatorcheckpoints.h"
 
 namespace AccumulatorCheckpoints
 {
