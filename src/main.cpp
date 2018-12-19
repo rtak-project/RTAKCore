2563b0bf06e74329c28b97beeef98415874a2d14
rtak-dev <rtak.repo@gmail.com>
2018-12-19 07:40:12 +0300

    Test reward

--- src/main.cpp
+++ src/main.cpp
@@ -1822,17 +1822,17 @@ int64_t GetBlockValue(int nHeight)
 
     if (nHeight == 0) {
       nSubsidy = 120000 * COIN;
-    } else if (nHeight > 0 && nHeight < 100) {
+    } else if (nHeight > 0 && nHeight <= 100) {
       nSubsidy = 10 * CENT;
-  	} else if (nHeight >= 101 && nHeight < 20000) {
+  	} else if (nHeight >= 101 && nHeight <= 20000) {
       nSubsidy = 200 * CENT;
-    } else if (nHeight >= 20001 && nHeight < 40000) {
+    } else if (nHeight >= 20001 && nHeight <= 40000) {
       nSubsidy = 150 * CENT;
-    } else if (nHeight >= 40001 && nHeight < 100000) {
+    } else if (nHeight >= 40001 && nHeight <= 100000) {
       nSubsidy = 110 * CENT;
-    } else if (nHeight >= 100001 && nHeight < 200000) {
+    } else if (nHeight >= 100001 && nHeight <= 200000) {
       nSubsidy = 120 * CENT;
-    } else if (nHeight >= 200001 && nHeight < 500000) {
+    } else if (nHeight >= 200001 && nHeight <= 500000) {
       nSubsidy = 80 * CENT;
     } else if (nHeight >= 500001) {
       nSubsidy = 60 * CENT;
@@ -1872,17 +1872,17 @@ int64_t GetMasternodePayment(int nHeight, int64_t blockValue, int nMasternodeCou
 {
     int64_t ret = 0;
 
-    if (nHeight > 0 && nHeight < 100) {
+    if (nHeight > 0 && nHeight <= 100) {
         ret = 40 * CENT;
-	  } else if (nHeight >= 101 && nHeight < 20000) {
+	  } else if (nHeight >= 101 && nHeight <= 20000) {
         ret = 880 * CENT;
-    } else if (nHeight >= 20001 && nHeight < 40000) {
+    } else if (nHeight >= 20001 && nHeight <= 40000) {
         ret = 720 * CENT;
-    } else if (nHeight >= 40001 && nHeight < 100000) {
+    } else if (nHeight >= 40001 && nHeight <= 100000) {
         ret = 600 * CENT;
-    } else if (nHeight >= 100001 && nHeight < 200000) {
+    } else if (nHeight >= 100001 && nHeight <= 200000) {
         ret = 450 * CENT;
-	  } else if (nHeight >= 200001 && nHeight < 500000) {
+	  } else if (nHeight >= 200001 && nHeight <= 500000) {
         ret = 300 * CENT;
     } else if (nHeight >= 500001) {
         ret = 200 * CENT;
