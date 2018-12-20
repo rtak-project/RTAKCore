2794757a6531f2217bf1ad329f3d6cb050bfa3a9
rtak-dev <rtak.repo@gmail.com>
2018-12-20 07:12:04 +0300

    Fix

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -33,7 +33,7 @@ unsigned int getIntervalVersion(bool fTestNet)
 
 // Hard checkpoints of stake modifiers to ensure they are deterministic
 static std::map<int, unsigned int> mapStakeModifierCheckpoints =
-    boost::assign::map_list_of(0, 0xfd11f4e7u);
+    boost::assign::map_list_of(0, 0x00000000);
 
 // Get time weight
 int64_t GetWeight(int64_t nIntervalBeginning, int64_t nIntervalEnd)
